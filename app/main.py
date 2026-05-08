e9a7b0f81e54051609bc6522aebf8f28a318b112
Daniel Alexander <26716312+DanielAlexanderDiaz@users.noreply.github.com>
2026-05-08 00:06:24 -0400

    init, main

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.db import init_db
+from api.routers.auth_router import router as auth_router
+
+load_dotenv()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+    
+app = FastAPI(
+    title="TicketFlow",
+    description="TicketFlow API",
+    version="1.0.0",
+    lifespan=lifespan,
+    swagger_ui_parameters={"persistAuthorization": True},
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router, prefix="/api/v1")
