50db42dec406d1dae1a638ead9c32cc2b99e1f96
Daniel Alexander <26716312+DanielAlexanderDiaz@users.noreply.github.com>
2026-05-08 00:00:10 -0400

    init models

---
+++ app/models/__init__.py
@@ -0,0 +1,15 @@
+from .usuario import Usuario, CrearUsuario, InfoUsuario
+from .ticket import Ticket, CrearTicket, InfoTicket
+from .comentario import ComentarioTicket, CrearComentarioTicket, InfoComentarioTicket
+
+__all__ = [
+    "Usuario",
+    "CrearUsuario",
+    "InfoUsuario",
+    "Ticket",
+    "CrearTicket",
+    "InfoTicket",
+    "ComentarioTicket",
+    "CrearComentarioTicket",
+    "InfoComentarioTicket",
+]
