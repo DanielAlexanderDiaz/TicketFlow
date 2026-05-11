7d604440dbd1b08084de69795461933494435d94
Daniel Alexander <26716312+DanielAlexanderDiaz@users.noreply.github.com>
2026-05-11 13:57:57 -0400

    mejorando modelo

--- app/models/comentario.py
+++ app/models/comentario.py
@@ -1,18 +1,21 @@
 import datetime
 from sqlmodel import Field, SQLModel
 
-class ComentarioTicket(SQLModel, table=True):
+class Comentario(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     id_ticket: int = Field(foreign_key="ticket.id", index=True)
     id_usuario: int = Field(foreign_key="usuario.id", index=True)
     comentario: str
     
-class CrearComentarioTicket(SQLModel):
+class CrearComentario(SQLModel):
     id_ticket: int
     id_usuario: int
     comentario: str
     
-class InfoComentarioTicket(SQLModel):
+class ActualizarComentario(SQLModel):
+    comentario: str
+    
+class InfoComentario(SQLModel):
     id: int
     id_ticket: int
     id_usuario: int
