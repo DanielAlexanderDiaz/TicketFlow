2fb8ea550f6f691c8d5f09e4ffbc4a0b33b40e45
Daniel Alexander <26716312+DanielAlexanderDiaz@users.noreply.github.com>
2026-05-12 14:03:54 -0400

    agregando dependencia de rol

--- app/api/dependencias.py
+++ app/api/dependencias.py
@@ -4,7 +4,7 @@ from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import Session
 from app.core.db import get_session
 from app.core.seguridad import decoder_token
-from app.models.usuario import Usuario
+from app.models.usuario import RoleEnum, Usuario
 from app.repositories.usuario_repository import UsuarioRepositorio
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/token")
@@ -31,3 +31,10 @@ def get_usuario_actual(token: Annotated[str, Depends(oauth2_scheme)], db: DBSess
     return usuario
 
 UsuarioActual = Annotated[Usuario, Depends(get_usuario_actual)]
+
+def require_admin(usuario: Usuario = Depends(UsuarioActual)):
+    if usuario.rol not in [RoleEnum.admin, RoleEnum.superuser]:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No autorizado")
+    return usuario
+
+PermisoAdmin = Annotated[Usuario, Depends(require_admin)]
