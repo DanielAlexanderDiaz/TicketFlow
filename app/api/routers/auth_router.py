b359f62df0a9f934b3668f778c6ac8a9c2f65583
Daniel Alexander <26716312+DanielAlexanderDiaz@users.noreply.github.com>
2026-05-18 00:38:58 -0400

    se corrige error de inicio de sesion

--- app/api/routers/auth_router.py
+++ app/api/routers/auth_router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
-from pydantic import EmailStr
 from app.api.dependencias import DBSession
-from app.schemas.usuario import CrearUsuario, InfoUsuario
+from app.schemas.usuario import CrearUsuario, InfoUsuario, LoginRequest
 from app.repositories.usuario_repository import UsuarioRepositorio
 from app.services.auth_services import AuthServices
 
@@ -14,15 +13,14 @@ def registrar(payload: CrearUsuario, db: DBSession):
     return servicio.registrar(payload)
 
 @router.post("/login")
-def login(email: EmailStr, password: str, db: DBSession):
+def login(payload: LoginRequest, db: DBSession):
     servicio = AuthServices(UsuarioRepositorio(db))
-    token = servicio.login(email, password)
-    return {"access_token": token, "token_type": "bearer"}
+    token_response = servicio.login(payload)
+    return {"access_token": token_response.access_token, "token_type": "bearer"}
 
 @router.post("/token")
 def login(db: DBSession, form: OAuth2PasswordRequestForm = Depends()):
-    email = form.username
-    password = form.password
+    payload = LoginRequest(email=form.username, password=form.password)
     servicio = AuthServices(UsuarioRepositorio(db))
-    token = servicio.login(email, password)
-    return {"access_token": token, "token_type": "bearer"}
+    token_response = servicio.login(payload)
+    return {"access_token": token_response.access_token, "token_type": "bearer"}
