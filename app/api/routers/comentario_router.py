a4fbddc34b5a879dc6b2b9ba4c3df10287d1f1f5
Daniel Alexander <26716312+DanielAlexanderDiaz@users.noreply.github.com>
2026-06-09 19:32:55 -0400

    actualizando exportaciones

--- app/api/routers/comentario_router.py
+++ app/api/routers/comentario_router.py
@@ -1,27 +1,27 @@
 from fastapi import APIRouter, status
 from app.api.dependencias import DBSession, UsuarioActual
-from app.schemas.comentario import ActualizarComentario, CrearComentario, InfoComentario
+from app.schemas.comentario import ActualizarComentario, CrearComentario, InformacionComentario
 from app.services.comentario_services import ComentarioService
 
 
 router = APIRouter(prefix="/comentario", tags=["comentario"])
 
-@router.get("/", response_model=InfoComentario)
+@router.get("/", response_model=InformacionComentario)
 def info_comentario(id_comentario: int, db: DBSession, usuario: UsuarioActual):
     return ComentarioService(db).comentario_by_id(id_comentario)
 
-@router.get("/{id_ticket}", response_model=list[InfoComentario])
+@router.get("/{id_ticket}", response_model=list[InformacionComentario])
 def info_comentarios(id_ticket: int, db: DBSession, usuario: UsuarioActual):
     return ComentarioService(db).comentario_by_ticket(id_ticket)
 
-@router.get("/usuario/{id_usuario}", response_model=list[InfoComentario])
+@router.get("/usuario/{id_usuario}", response_model=list[InformacionComentario])
 def info_comentarios(db: DBSession, usuario: UsuarioActual):
     return ComentarioService(db).comentario_by_usuario(usuario.id)
 
-@router.post("/{id_ticket}", response_model=InfoComentario, status_code=status.HTTP_201_CREATED)
+@router.post("/{id_ticket}", response_model=InformacionComentario, status_code=status.HTTP_201_CREATED)
 def crear_comentario(id_ticket: int, payload: CrearComentario, db: DBSession, usuario: UsuarioActual):
     return ComentarioService(db).crear_comentario(id_ticket, usuario.id, payload)
 
-@router.patch("/{id_comentario}", response_model=InfoComentario)
+@router.patch("/{id_comentario}", response_model=InformacionComentario)
 def actualizar_comentario(id_comentario: int, payload: ActualizarComentario, db: DBSession, usuario: UsuarioActual):
     return ComentarioService(db).actualizar_comentario(id_comentario, usuario.id, payload)
