0490da8213a93281962aa0377bfc17ce2e51bf39
Daniel Alexander <26716312+DanielAlexanderDiaz@users.noreply.github.com>
2026-06-09 19:33:40 -0400

    actualizando exportaciones

--- app/api/routers/ticket_router.py
+++ app/api/routers/ticket_router.py
@@ -1,18 +1,17 @@
 from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException, Query, status, File, UploadFile
-from app.api.dependencias import DBSession, UsuarioActual, requiere_admin, puede_gestionar_ticket, puede_crear_ticket
+from app.api.dependencias import DBSession, UsuarioActual, requiere_admin, puede_gestionar_ticket
 from app.models.ticket import EstadoTicket, PrioridadTicket
-from app.schemas.ticket import ActualizarEstadoTicket, ActualizarTickekActivo, InfoTicket, ActualizarTicket, CrearTicket, HistorialTicket, PaginacionTicket
-from app.services.ticket_estado_services import TicketEstadoServices
+from app.schemas.ticket import CrearTicket, ActualizarTicket, EliminarTicket, CambioEstadoTicket, AsignarTicket, CompartirTicket, EliminarCompartirTicket, TicketActivo, InformacionTicket, PaginacionTicket
 from app.services.ticket_services import TicketService
 
 router = APIRouter(prefix="/ticket", tags=["ticket"])
 
-@router.get("/listar", response_model=list[InfoTicket])
+@router.get("/listar", response_model=list[InformacionTicket])
 def listar_tickets_usuario(db: DBSession, usuario: UsuarioActual):
     return TicketService(db).ticket_by_usuario(usuario.id)
 
-@router.get("/listar_todos", response_model=list[InfoTicket], dependencies=[Depends(requiere_admin)])
+@router.get("/listar_todos", response_model=list[InformacionTicket], dependencies=[Depends(requiere_admin)])
 def listar_todos_los_tickets(db: DBSession):
     return TicketService(db).listar_tickets()
 
@@ -28,34 +27,28 @@ def listar_ticket_paginado(
     ):
     return TicketService(db).listar_ticket_paginado(estado, prioridad, activo, titulo, page, size)
 
-@router.post("/", response_model=InfoTicket, status_code=status.HTTP_201_CREATED, dependencies=[Depends(puede_crear_ticket)])
+@router.post("/", response_model=InformacionTicket, status_code=status.HTTP_201_CREATED, dependencies=[Depends(puede_gestionar_ticket)])
 def crear_ticket(payload: CrearTicket, db: DBSession, usuario: UsuarioActual):
     return TicketService(db).crear_ticket(usuario.id, payload)
 
-@router.get("/", response_model=InfoTicket)
+@router.get("/", response_model=InformacionTicket)
 def informacion_del_ticket(id_ticket: int, db: DBSession, usuario: UsuarioActual):
     return TicketService(db).ticket_by_id(id_ticket, usuario.id)
 
-@router.patch("/{id_ticket}", response_model=InfoTicket, dependencies=[Depends(puede_gestionar_ticket)])
+@router.patch("/{id_ticket}", response_model=InformacionTicket, dependencies=[Depends(puede_gestionar_ticket)])
 def actualizar_ticket(id_ticket: int, payload: ActualizarTicket, db: DBSession, usuario: UsuarioActual):
     return TicketService(db).actualizar_ticket(id_ticket, payload, usuario.id)
 
-@router.patch("/{id_ticket}/vigencia", response_model=InfoTicket, dependencies=[Depends(puede_gestionar_ticket)])
-def actualizar_ticket_activo(id_ticket: int, payload: ActualizarTickekActivo, db: DBSession, usuario: UsuarioActual):
+@router.patch("/{id_ticket}/vigencia", response_model=InformacionTicket, dependencies=[Depends(puede_gestionar_ticket)])
+def actualizar_ticket_activo(id_ticket: int, payload: TicketActivo, db: DBSession, usuario: UsuarioActual):
     return TicketService(db).actualizar_ticket_activo(id_ticket, usuario.id, payload)
 
-@router.patch("/{id_ticket}/imagen", response_model=InfoTicket, dependencies=[Depends(puede_gestionar_ticket)])
+@router.patch("/{id_ticket}/imagen", response_model=InformacionTicket, dependencies=[Depends(puede_gestionar_ticket)])
 def actualizar_imagen_ticket(db: DBSession, usuario: UsuarioActual,id_ticket: int, img: Optional[UploadFile] = File(None)):
     payload = ActualizarTicket(imagen_url=img)
     return TicketService(db).actualizar_ticket(id_ticket, payload, usuario.id)
 
-@router.patch("/{id_ticket}/estado", response_model=InfoTicket, dependencies=[Depends(puede_gestionar_ticket)])
-def actualizar_cambiar_estado(db: DBSession, id_ticket: int, new_estado: ActualizarEstadoTicket, usuario: UsuarioActual):
-    return TicketEstadoServices(db).cambiar_estado_ticket(id_ticket, new_estado)
+@router.patch("/{id_ticket}/estado", response_model=InformacionTicket, dependencies=[Depends(puede_gestionar_ticket)])
+def actualizar_cambiar_estado(db: DBSession, id_ticket: int, new_estado: CambioEstadoTicket, usuario: UsuarioActual):
+    return TicketService(db).cambio_estado_ticket(id_ticket, new_estado)
 
-@router.get("/{id_ticket}/historial", response_model=list[HistorialTicket], dependencies=[Depends(requiere_admin)])
-def obtener_historial(id_ticket: int, db: DBSession):
-    try:   
-        return TicketService(db).obtener_historial(id_ticket)
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
